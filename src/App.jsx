6daf87bac47331d620aadf2feff9a9de48042fab
thais <thais3225@gmail.com>
2025-07-06 09:33:12 -0300

    pequenas mudanças na UI + delete da pasta assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import profileImage from './assets/images/profile/profile.png'
+import profileImage from '../public/profile/profile.png'
 import { useState } from 'react';
 
 function App() {
@@ -10,7 +10,6 @@ function App() {
       <div className="header-left">
         <span className="logo">SyncWithU</span>
       </div>
-
       <div className="header-right">
         <nav className="header-nav">
           <a href="#" onClick={() => {setIsEditing(!isEditing )}}>{isEditing ?  "Save" : "Edit Profile"}</a>
@@ -28,6 +27,7 @@ function App() {
                 <input type="file" style={{ display: "none" }} />
                 <span className="edit-photo-btn">Trocar foto</span>
               </label>
+              <input className="edit-input age" defaultValue="20" />
               <input className="edit-input name" defaultValue="Theodora Coelho" />
               <input className="edit-input job" defaultValue="Doctor specializing in mental health" />
               <input className="edit-input address" defaultValue="Rua Ricardo José, 200, Visconde do Rio Branco" />
@@ -35,7 +35,10 @@ function App() {
           ) : (
             <>
               <img src={profileImage} alt="profile photo" />
-              <h2 className='name-above-photo'>Theodora Coelho</h2>
+              <div className="name-age-row">
+                <h2 className='name-above-photo'>Theodora Coelho</h2>
+                <span className='age'>20 anos</span>
+              </div>
               <p className='job-address'>Doctor specializing in mental health</p>
               <p className='job-address'>Rua Ricardo José, 200, Visconde do Rio Branco</p>
             </>
@@ -43,7 +46,7 @@ function App() {
         </div>
       </div>  
       <div>
-        <h2 className='about'>About</h2>
+        <h2 className='about'>Biography</h2>
         {isEditing ? (
           <textarea className="edit-textarea" defaultValue="Experienced software engineer with a passion for creating innovative solutions. Skilled in various programming languages and frameworks, with a focus on developing scalable and efficient applications. Committed to continuous learning and staying up-to-date with the latest industry trends." />
         ) : (
