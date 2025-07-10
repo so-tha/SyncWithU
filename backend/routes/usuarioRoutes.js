a078131a1063334527abc524802647cb2cec189e
thais <thais3225@gmail.com>
2025-07-10 09:53:30 -0300

    criação das rotas para as imagens

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import UsuarioController from '../controllers/usuarioController.js';
+import upload from '../middleware/upload.js';
 
 const router = express.Router();
 
@@ -9,5 +10,6 @@ router.post('/usuario', UsuarioController.criar);
 router.put('/usuario/:id', UsuarioController.atualizar);
 router.delete('/usuario/:id', UsuarioController.deletar);
 router.post('/login', UsuarioController.login);
+router.post('/usuario/:id/foto', upload.single('foto'), UsuarioController.uploadFoto);
 
 export default router; 
