1709e8cf376e6b6056c542ae072cb0e1df530d0b
thais <thais3225@gmail.com>
2025-07-10 07:35:35 -0300

    arquivos de rotas dos usuarios

---
+++ backend/routes/usuarioRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import UsuarioController from '../controllers/usuarioController.js';
+
+const router = express.Router();
+
+router.get('/usuario', UsuarioController.listarTodos);
+router.get('/usuario/:id', UsuarioController.buscarPorId);
+router.post('/usuario', UsuarioController.criar);
+router.put('/usuario/:id', UsuarioController.atualizar);
+router.delete('/usuario/:id', UsuarioController.deletar);
+
+export default router; 
