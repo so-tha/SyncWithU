efa3f3bfe5fa334f33831121cd4bda04fb0cd63d
thais <thais3225@gmail.com>
2025-07-10 14:56:48 -0300

    Correção no erro do Cors para permitir as requisições vindas do front

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 //Middlewares necessários
-app.use(cors());
+app.use(cors({
+  origin: 'https://sync-with-u.vercel.app', 
+  credentials: true 
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('public/uploads'));
